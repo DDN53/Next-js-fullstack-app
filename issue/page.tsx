4277a0da2c3583041ee003a9cddb376a94ee87e5
Ddn53 <dinukanished9@gmail.com>
2024-07-17 17:21:02 +0530

    handle form submittion

---
+++ issue/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Text, Button, Link } from "@radix-ui/themes";
+
+export default function page() {
+  return (
+    <div>
+      <Button>
+        <Link href="/issue/new">New issue</Link>
+      </Button>
+    </div>
+  );
+}
